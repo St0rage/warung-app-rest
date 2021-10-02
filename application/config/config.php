5801be9a2ecf9fea98644947902569c1732c9370
St0rage <daniyudistira25@gmail.com>
2021-10-02 20:43:40 +0700

    ready2

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.3.5/warung-app-rest/';
+$config['base_url'] = 'http://warung-rahmat-gas.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
